ee25ff5a093331d9c42bc9fe56c5dcc7d630be0d
Thomas Roch <thomas.c.roch@gmail.com>
2015-06-29 17:12:35 +0100

    chore: add common js build

--- config.js
+++ config.js
@@ -18,17 +18,42 @@ System.config({
     "babel": "npm:babel-core@5.6.15",
     "babel-runtime": "npm:babel-runtime@5.6.15",
     "core-js": "npm:core-js@0.9.18",
+    "deku": "npm:deku@0.4.5",
     "route-node": "npm:route-node@0.0.3",
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.2.2"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@5.6.15": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
+    "npm:buffer@3.2.2": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "ieee754": "npm:ieee754@1.1.6",
+      "is-array": "npm:is-array@1.0.1"
+    },
     "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:deku@0.4.5": {
+      "array-flatten": "npm:array-flatten@1.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "component-emitter": "npm:component-emitter@1.2.0",
+      "component-raf": "npm:component-raf@1.2.0",
+      "component-type": "npm:component-type@1.1.0",
+      "dom-pool": "npm:dom-pool@0.1.1",
+      "dom-walk": "npm:dom-walk@0.1.1",
+      "fast.js": "npm:fast.js@0.1.1",
+      "get-uid": "npm:get-uid@1.0.1",
+      "is-dom": "npm:is-dom@1.0.5",
+      "is-promise": "npm:is-promise@2.0.0",
+      "object-path": "npm:object-path@0.8.1",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "sliced": "npm:sliced@0.0.5"
     },
     "npm:route-node@0.0.2": {
       "path-parser": "npm:path-parser@0.0.4"
---
+++ index.js
@@ -0,0 +1,111 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _routeNode = require('route-node');
+
+var _routeNode2 = _interopRequireDefault(_routeNode);
+
+var nameToIDs = function nameToIDs(name) {
+    return name.split('.').reduce(function (ids, name) {
+        ids.push(ids.length ? ids[ids.length - 1] + '.' + name : name);
+        return ids;
+    }, []);
+};
+
+var Router5 = (function () {
+    function Router5(routes) {
+        var _this = this;
+
+        _classCallCheck(this, Router5);
+
+        this.callbacks = [];
+        this.lastStateAttempt = null;
+        this.lastKnownState = null;
+        this.rootNode = routes instanceof _routeNode2['default'] ? routes : new _routeNode2['default']('', '', routes);
+        this.activeComponents = {};
+
+        window.addEventListener('popstate', function (evt) {
+            _this.lastStateAttempt = evt.state;
+            _this._invokeCallbacks(evt.state, _this.lastKnownState);
+        });
+    }
+
+    _createClass(Router5, [{
+        key: '_invokeCallbacks',
+        value: function _invokeCallbacks(newState, oldState) {
+            var _this2 = this;
+
+            this.callbacks.forEach(function (cb) {
+                cb.call(_this2, newState, oldState);
+            });
+        }
+    }, {
+        key: 'addListener',
+        value: function addListener(cb) {
+            this.callbacks.push(cb);
+        }
+    }, {
+        key: 'removeListener',
+        value: function removeListener(cb) {
+            this.callbacks = this.callbacks.filter(function (callback) {
+                return callback !== cb;
+            });
+        }
+    }, {
+        key: 'buildPath',
+        value: function buildPath(route, params) {
+            return this.rootNode.buildPath(route, params);
+        }
+    }, {
+        key: 'navigate',
+        value: function navigate(name) {
+            var params = arguments[1] === undefined ? {} : arguments[1];
+            var opts = arguments[2] === undefined ? {} : arguments[2];
+
+            var currentState = window.history.state;
+            // let segments = this.rootNode.getSegmentsByName(name)
+            // let path  = this.rootNode.buildPathFromSegments(segments, params)
+            var path = this.rootNode.buildPath(name, params);
+
+            if (!path) {
+                throw new Error('Could not find route "' + name + '"');
+            }
+
+            this.lastStateAttempt = { name: name, path: path, params: params };
+
+            if (this.lastKnownState) {
+                var i = undefined;
+                // Diff segments
+                var segmentIds = nameToIDs(name);
+                var activeSegmentIds = nameToIDs(this.lastKnownState.name);
+                var maxI = Math.min(segmentIds.length, activeSegmentIds.length);
+                for (i = 0; i < maxI; i += 1) {
+                    if (activeSegmentIds[i] !== segmentIds[i]) break;
+                }
+                var segmentsToDeactivate = activeSegmentIds.slice(i);
+                console.log('to deactivate: ', segmentsToDeactivate);
+            }
+            // Push to history
+            window.history[opts.replace ? 'replaceState' : 'pushState'](this.lastStateAttempt, '', path);
+            // Update lastKnowState
+            this._invokeCallbacks(this.lastStateAttempt, this.lastKnownState);
+
+            this.lastKnownState = this.lastStateAttempt;
+        }
+    }]);
+
+    return Router5;
+})();
+
+exports['default'] = Router5;
+module.exports = exports['default'];
+
