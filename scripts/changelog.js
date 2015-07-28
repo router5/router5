cfc558c19e9986afc0afd81cf19453608c0a0d6c
Thomas Roch <thomas.c.roch@gmail.com>
2015-07-28 10:23:56 +0100

    chore: update changelog script

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,7 +1,9 @@
-var clog   = require('conventional-changelog');
-var path   = require('path');
-var fs     = require('vinyl-fs');
-var source = require('vinyl-source-stream');
+var clog    = require('conventional-changelog');
+var path    = require('path');
+var vfs     = require('vinyl-fs');
+var fs      = require('fs')
+var source  = require('vinyl-source-stream2');
+var through = require('through2');
 
 function conventionalChangelog(done) {
     clog({
@@ -10,7 +12,16 @@ function conventionalChangelog(done) {
             // append: false
         })
         .pipe(source('CHANGELOG.md'))
-        .pipe(fs.dest(path.join(__dirname, '..')));
+        .pipe(through.obj(function (file, enc, cb) {
+            fs.readFile(path.join(__dirname, '../CHANGELOG.md'), function (err, res) {
+                if (err) cb(err);
+                else {
+                    file.contents = new Buffer(file.contents.toString() + res.toString());
+                    cb(null, file);
+                }
+            });
+        }))
+        .pipe(vfs.dest(path.join(__dirname, '..')));
 }
 
 conventionalChangelog(function (err) {
