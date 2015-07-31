5d344a26faf5a44aae0344fea07ad34e38bdf25e
Thomas Roch <thomas.c.roch@gmail.com>
2015-07-31 17:04:44 +0100

    fix: detect left click properly on links

--- modules/link-factory.js
+++ modules/link-factory.js
@@ -40,7 +40,7 @@ function linkFactory(router) {
         },
 
         clickHandler(evt) {
-            if (evt.which === 1) {
+            if (evt.nativeEvent.which === 1) {
                 evt.preventDefault()
                 router.navigate(this.props.routeName, this.props.routeParams, this.props.routeOptions)
             }
