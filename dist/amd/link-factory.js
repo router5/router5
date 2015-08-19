6092808ecc12d215114f26ffe2744d265fc05fb4
Thomas Roch <thomas.c.roch@gmail.com>
2015-08-19 08:37:00 +0100

    fix: missing dist files for 0.1.2

--- dist/amd/link-factory.js
+++ dist/amd/link-factory.js
@@ -42,7 +42,7 @@ define(['exports', 'module', 'react'], function (exports, module, _react) {
             },
 
             clickHandler: function clickHandler(evt) {
-                if (evt.nativeEvent.which === 1) {
+                if (!evt.nativeEvent.which === 2) {
                     evt.preventDefault();
                     router.navigate(this.props.routeName, this.props.routeParams, this.props.routeOptions);
                 }
--- dist/browser/router5-react.js
+++ dist/browser/router5-react.js
@@ -60,7 +60,7 @@ function linkFactory(router) {
         },
 
         clickHandler: function clickHandler(evt) {
-            if (evt.nativeEvent.which === 1) {
+            if (!evt.nativeEvent.which === 2) {
                 evt.preventDefault();
                 router.navigate(this.props.routeName, this.props.routeParams, this.props.routeOptions);
             }
