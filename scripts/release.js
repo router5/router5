cf38dbb53513c16a0979363f76e1081b89542fa9
Thomas Roch <thomas.roch@fanduel.com>
2015-10-17 11:21:56 +0100

    chore: fix release script

--- scripts/release.js
+++ scripts/release.js
@@ -8,13 +8,13 @@ var versionType = argv.major ? 'major' : (argv.minor ? 'minor' : 'patch');
 function run(cmd, log) {
     log = log === undefined ? true : log;
     var res = exec(cmd).toString();
-    if (log) console.log(res);
+    if (log && res) console.log(res);
     return res;
 }
 
 run('npm install');
 run('npm prune');
-var VERSION + run('npm --no-git-tag-version version ' + versionType, false);
+var VERSION = run('npm --no-git-tag-version version ' + versionType, false);
 
 run('npm run lint');
 run('npm run build');
@@ -25,8 +25,8 @@ run('npm run clog');
 run('git add -A');
 run('git add dist -f');
 run('git commit -m "chore: ' + VERSION + '"');
-// run('git tag ' + VERSION);
-// run('git push origin master');
-// run('git push --tags');
+run('git tag ' + VERSION);
+run('git push origin master');
+run('git push --tags');
 
-// run('npm publish');
+run('npm publish');
