b49c33f868743ee873370c924d6a43ced03241a8
Thomas Roch <thomas.c.roch@gmail.com>
2016-01-06 14:19:38 +0000

    chore: v1.4.0

--- dist/browser/router5.js
+++ dist/browser/router5.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * @version 1.3.0
+ * @version 1.4.0
  * The MIT License (MIT)
  * 
  * Copyright (c) 2015 Thomas Roch
@@ -141,6 +141,12 @@
     }, {
         // Query parameter: ?param1&param2
         //                   ?:param1&:param2
+        name: 'query-parameter-bracket',
+        pattern: /^(?:\?|&)(?:\:)?([a-zA-Z0-9-_]*[a-zA-Z0-9]{1})(?:\[\])/
+    }, // regex:   match => new RegExp('(?=(\?|.*&)' + match[0] + '(?=(\=|&|$)))')
+    {
+        // Query parameter: ?param1&param2
+        //                   ?:param1&:param2
         name: 'query-parameter',
         pattern: /^(?:\?|&)(?:\:)?([a-zA-Z0-9-_]*[a-zA-Z0-9]{1})/
     }, // regex:   match => new RegExp('(?=(\?|.*&)' + match[0] + '(?=(\=|&|$)))')
@@ -161,7 +167,7 @@
     }, {
         // Unmatched fragment (until delimiter is found)
         name: 'fragment',
-        pattern: /^([0-9a-zA-Z]+?)/,
+        pattern: /^([0-9a-zA-Z]+)/,
         regex: function regex(match) {
             return new RegExp(match[0]);
         }
@@ -186,6 +192,7 @@
             if (match[0].length < str.length) tokens = tokenise(str.substr(match[0].length), tokens);
             return true;
         });
+    
         // If no rules matched, throw an error (possible malformed path)
         if (!matched) {
             throw new Error('Could not parse path.');
@@ -199,9 +206,17 @@
         return source.replace(/\\\/$/, '') + '(?:\\/)?';
     };
     
+    var withoutBrackets = function withoutBrackets(param) {
+        return param.replace(/\[\]$/, '');
+    };
+    
     var appendQueryParam = function appendQueryParam(params, param) {
         var val = arguments.length <= 2 || arguments[2] === undefined ? '' : arguments[2];
     
+        if (/\[\]$/.test(param)) {
+            param = withoutBrackets(param);
+            val = [val];
+        }
         var existingVal = params[param];
     
         if (existingVal === undefined) params[param] = val;else params[param] = Array.isArray(existingVal) ? existingVal.concat(val) : [existingVal, val];
@@ -212,6 +227,7 @@
     var parseQueryParams = function parseQueryParams(path) {
         var searchPart = path.split('?')[1];
         if (!searchPart) return {};
+    
         return searchPart.split('&').map(function (_) {
             return _.split('=');
         }).reduce(function (obj, m) {
@@ -262,7 +278,8 @@
                 );
             }).length > 0;
             this.hasQueryParams = this.tokens.filter(function (t) {
-                return t.type === 'query-parameter';
+                return (/^query-parameter/.test(t.type)
+                );
             }).length > 0;
             // Extract named parameters from tokens
             this.urlParams = !this.hasUrlParams ? [] : this.tokens.filter(function (t) {
@@ -280,12 +297,20 @@
                 return t.type === 'query-parameter';
             }).map(function (t) {
                 return t.val;
-            })
-            // Flatten
-            .reduce(function (r, v) {
+            }).reduce(function (r, v) {
                 return r.concat(v);
-            });
-            this.params = this.urlParams.concat(this.queryParams);
+            }, []);
+    
+            this.queryParamsBr = !this.hasQueryParams ? [] : this.tokens.filter(function (t) {
+                return (/-bracket$/.test(t.type)
+                );
+            }).map(function (t) {
+                return t.val;
+            }).reduce(function (r, v) {
+                return r.concat(v);
+            }, []);
+    
+            this.params = this.urlParams.concat(this.queryParams).concat(this.queryParamsBr);
             // Check if hasQueryParams
             // Regular expressions for url part only (full and partial match)
             this.source = this.tokens.filter(function (t) {
@@ -318,22 +343,22 @@
                 // trailingSlash: falsy => non optional, truthy => optional
                 var source = optTrailingSlash(this.source, trailingSlash);
                 // Check if exact match
-                var match = this._urlMatch(path, new RegExp('^' + source + (this.hasQueryParams ? '\\?.*$' : '$')));
+                var matched = this._urlMatch(path, new RegExp('^' + source + (this.hasQueryParams ? '(\\?.*$|$)' : '$')));
                 // If no match, or no query params, no need to go further
-                if (!match || !this.hasQueryParams) return match;
+                if (!matched || !this.hasQueryParams) return matched;
                 // Extract query params
                 var queryParams = parseQueryParams(path);
                 var unexpectedQueryParams = Object.keys(queryParams).filter(function (p) {
-                    return _this2.queryParams.indexOf(p) === -1;
+                    return _this2.queryParams.concat(_this2.queryParamsBr).indexOf(p) === -1;
                 });
     
                 if (unexpectedQueryParams.length === 0) {
                     // Extend url match
                     Object.keys(queryParams).forEach(function (p) {
-                        return match[p] = queryParams[p];
+                        return matched[p] = queryParams[p];
                     });
     
-                    return match;
+                    return matched;
                 }
     
                 return null;
@@ -357,7 +382,7 @@
                 var queryParams = parseQueryParams(path);
     
                 Object.keys(queryParams).filter(function (p) {
-                    return _this3.queryParams.indexOf(p) >= 0;
+                    return _this3.queryParams.concat(_this3.queryParamsBr).indexOf(p) >= 0;
                 }).forEach(function (p) {
                     return appendQueryParam(match, p, queryParams[p]);
                 });
@@ -388,19 +413,24 @@
                 }
     
                 var base = this.tokens.filter(function (t) {
-                    return t.type !== 'query-parameter';
+                    return (/^query-parameter/.test(t.type) === false
+                    );
                 }).map(function (t) {
-                    if (t.type === 'url-parameter-matrix') return ';' + t.val[0] + '=' + params[t.val[0]];
+                    if (t.type === 'url-parameter-matrix') return ';' + t.val + '=' + params[t.val[0]];
                     return (/^url-parameter/.test(t.type) ? params[t.val[0]] : t.match
                     );
                 }).join('');
     
                 if (opts.ignoreSearch) return base;
     
-                var searchPart = this.queryParams.filter(function (p) {
-                    return Object.keys(params).indexOf(p) !== -1;
+                var queryParams = this.queryParams.concat(this.queryParamsBr.map(function (p) {
+                    return p + '[]';
+                }));
+    
+                var searchPart = queryParams.filter(function (p) {
+                    return Object.keys(params).indexOf(withoutBrackets(p)) !== -1;
                 }).map(function (p) {
-                    return _serialise(p, params[p]);
+                    return _serialise(p, params[withoutBrackets(p)]);
                 }).join('&');
     
                 return base + (searchPart ? '?' + searchPart : '');
@@ -417,6 +447,11 @@
         return val !== undefined && val !== null && val !== '';
     };
     
+    var bracketTest = /\[\]$/;
+    var withoutBrackets = function withoutBrackets(param) {
+        return param.replace(bracketTest, '');
+    };
+    
     var removeQueryParamsFromPath = function removeQueryParamsFromPath(path, params) {
         if (path.indexOf('?') === -1) return path;
         var splitPath = path.split('?');
@@ -425,9 +460,12 @@
     
         var remainingSearchParams = searchPart.split('&').reduce(function (obj, p) {
             var splitParam = p.split('=');
+            var hasBrackets = bracketTest.test(splitParam[0]);
             var key = splitParam[0];
             var val = decodeURIComponent(splitParam[1]);
-            if (params.indexOf(key) === -1) obj[key] = val || '';
+            val = hasBrackets ? [val] : val;
+    
+            if (params.indexOf(withoutBrackets(key)) === -1) obj[key] = val || '';
             return obj;
         }, {});
     
@@ -499,26 +537,28 @@
                 if (names.length === 1) {
                     this.children.push(route);
                     // Push greedy spats to the bottom of the pile
-                    this.children.sort(function (a, b) {
+                    this.children.sort(function (left, right) {
+                        var leftPath = left.path.split('?')[0];
+                        var rightPath = right.path.split('?')[0];
                         // '/' last
-                        if (a.path === '/') return 1;
-                        if (b.path === '/') return -1;
-                        var aHasParams = a.parser.hasUrlParams || a.parser.hasSpatParam;
-                        var bHasParams = b.parser.hasUrlParams || b.parser.hasSpatParam;
+                        if (leftPath === '/') return 1;
+                        if (rightPath === '/') return -1;
+                        var leftHasParams = left.parser.hasUrlParams || left.parser.hasSpatParam;
+                        var rightHasParams = right.parser.hasUrlParams || right.parser.hasSpatParam;
                         // No params first, sort by length descending
-                        if (!aHasParams && !bHasParams) {
-                            return a.path && b.path ? a.path.length < b.path.length ? 1 : -1 : 0;
+                        if (!leftHasParams && !rightHasParams) {
+                            return leftPath && rightPath ? leftPath.length < rightPath.length ? 1 : -1 : 0;
                         }
                         // Params last
-                        if (aHasParams && !bHasParams) return 1;
-                        if (!aHasParams && bHasParams) return -1;
+                        if (leftHasParams && !rightHasParams) return 1;
+                        if (!leftHasParams && rightHasParams) return -1;
                         // Spat params last
-                        if (!a.parser.hasSpatParam && b.parser.hasSpatParam) return -1;
-                        if (!b.parser.hasSpatParam && a.parser.hasSpatParam) return 1;
+                        if (!left.parser.hasSpatParam && right.parser.hasSpatParam) return -1;
+                        if (!right.parser.hasSpatParam && left.parser.hasSpatParam) return 1;
                         // Sort by number of segments descending
-                        var aSegments = (a.path.match(/\//g) || []).length;
-                        var bSegments = (b.path.match(/\//g) || []).length;
-                        if (aSegments < bSegments) return 1;
+                        var leftSegments = (leftPath.match(/\//g) || []).length;
+                        var rightSegments = (rightPath.match(/\//g) || []).length;
+                        if (leftSegments < rightSegments) return 1;
                         return 0;
                     });
                 } else {
@@ -575,8 +615,7 @@
                         var child = nodes[i];
                         // Partially match path
                         var match = child.parser.partialMatch(pathSegment);
-                        var remainingPath = undefined,
-                            remainingSearch = undefined;
+                        var remainingPath = undefined;
     
                         if (!match && trailingSlash) {
                             // Try with optional trailing slash
@@ -585,7 +624,7 @@
                         } else if (match) {
                             // Remove consumed segment from path
                             var consumedPath = child.parser.build(match, { ignoreSearch: true });
-                            remainingPath = removeQueryParamsFromPath(pathSegment.replace(consumedPath, ''), child.parser.queryParams);
+                            remainingPath = removeQueryParamsFromPath(pathSegment.replace(consumedPath, ''), child.parser.queryParams.concat(child.parser.queryParamsBr));
     
                             if (trailingSlash && remainingPath === '/' && !/\/$/.test(consumedPath)) {
                                 remainingPath = '';
@@ -654,16 +693,16 @@
     
                 var searchParams = segments.filter(function (s) {
                     return s.parser.hasQueryParams;
-                }).map(function (s) {
-                    return s.parser.queryParams;
-                });
+                }).reduce(function (params, s) {
+                    return params.concat(s.parser.queryParams).concat(s.parser.queryParamsBr.map(function (p) {
+                        return p + '[]';
+                    }));
+                }, []);
     
-                var searchPart = !searchParams.length ? null : searchParams.reduce(function (queryParams, params) {
-                    return queryParams.concat(params);
-                }).filter(function (p) {
-                    return Object.keys(params).indexOf(p) !== -1;
+                var searchPart = !searchParams.length ? null : searchParams.filter(function (p) {
+                    return Object.keys(params).indexOf(withoutBrackets(p)) !== -1;
                 }).map(function (p) {
-                    return Path.serialise(p, params[p]);
+                    return Path.serialise(p, params[withoutBrackets(p)]);
                 }).join('&');
     
                 return segments.map(function (segment) {
