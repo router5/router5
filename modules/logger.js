89227b49586bf382f49daa4a080c2377ac080739
Thomas Roch <thomas.c.roch@gmail.com>
2016-01-19 09:28:50 +0000

    refactor: refactor logger plugin

--- modules/logger.js
+++ modules/logger.js
@@ -1,36 +1,37 @@
 /* istanbul ignore next */
-function loggerPlugin() {
-    const startGroup = () => console.group('Router transition');
-    const endGroup = () => console.groupEnd('Router transition');
+const loggerPlugin = () =>
+    () => {
+        const startGroup = () => console.group('Router transition');
+        const endGroup = () => console.groupEnd('Router transition');
 
-    return {
-        name: 'LOGGER',
-        onStart() {
-            console.info('Router started');
-        },
-        onStop() {
-            console.info('Router stopped');
-        },
-        onTransitionStart(toState, fromState) {
-            endGroup();
-            startGroup();
-            console.log('Transition started from state');
-            console.log(fromState);
-            console.log('To state');
-            console.log(toState);
-        },
-        onTransitionCancel() {
-            console.warn('Transition cancelled');
-        },
-        onTransitionError(toState, fromState, err) {
-            console.warn('Transition error with code ' + err.code);
-            endGroup();
-        },
-        onTransitionSuccess() {
-            console.log('Transition success');
-            endGroup();
-        }
+        return {
+            name: 'LOGGER',
+            onStart() {
+                console.info('Router started');
+            },
+            onStop() {
+                console.info('Router stopped');
+            },
+            onTransitionStart(toState, fromState) {
+                endGroup();
+                startGroup();
+                console.log('Transition started from state');
+                console.log(fromState);
+                console.log('To state');
+                console.log(toState);
+            },
+            onTransitionCancel() {
+                console.warn('Transition cancelled');
+            },
+            onTransitionError(toState, fromState, err) {
+                console.warn('Transition error with code ' + err.code);
+                endGroup();
+            },
+            onTransitionSuccess() {
+                console.log('Transition success');
+                endGroup();
+            }
+        };
     };
-}
 
 export default loggerPlugin;
