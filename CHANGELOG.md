83e13d9c80555c7be450ca9a3efffd610570fbe5
Thomas Roch <thomas.c.roch@gmail.com>
2016-01-19 10:52:15 +0000

    chore: v2.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+<a name="2.0.1"></a>
+## [2.0.1](https://github.com/router5/router5/compare/v2.0.0...v2.0.1) (2016-01-19)
+
+
+
+
 <a name="2.0.0-rc.1"></a>
 # 2.0.0-rc.1 (2016-01-18)
 
--- dist/commonjs/logger.js
+++ dist/commonjs/logger.js
@@ -4,43 +4,45 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 /* istanbul ignore next */
-function loggerPlugin() {
-    var startGroup = function startGroup() {
-        return console.group('Router transition');
-    };
-    var endGroup = function endGroup() {
-        return console.groupEnd('Router transition');
-    };
+var loggerPlugin = function loggerPlugin() {
+    return function () {
+        var startGroup = function startGroup() {
+            return console.group('Router transition');
+        };
+        var endGroup = function endGroup() {
+            return console.groupEnd('Router transition');
+        };
 
-    return {
-        name: 'LOGGER',
-        onStart: function onStart() {
-            console.info('Router started');
-        },
-        onStop: function onStop() {
-            console.info('Router stopped');
-        },
-        onTransitionStart: function onTransitionStart(toState, fromState) {
-            endGroup();
-            startGroup();
-            console.log('Transition started from state');
-            console.log(fromState);
-            console.log('To state');
-            console.log(toState);
-        },
-        onTransitionCancel: function onTransitionCancel() {
-            console.warn('Transition cancelled');
-        },
-        onTransitionError: function onTransitionError(toState, fromState, err) {
-            console.warn('Transition error with code ' + err.code);
-            endGroup();
-        },
-        onTransitionSuccess: function onTransitionSuccess() {
-            console.log('Transition success');
-            endGroup();
-        }
+        return {
+            name: 'LOGGER',
+            onStart: function onStart() {
+                console.info('Router started');
+            },
+            onStop: function onStop() {
+                console.info('Router stopped');
+            },
+            onTransitionStart: function onTransitionStart(toState, fromState) {
+                endGroup();
+                startGroup();
+                console.log('Transition started from state');
+                console.log(fromState);
+                console.log('To state');
+                console.log(toState);
+            },
+            onTransitionCancel: function onTransitionCancel() {
+                console.warn('Transition cancelled');
+            },
+            onTransitionError: function onTransitionError(toState, fromState, err) {
+                console.warn('Transition error with code ' + err.code);
+                endGroup();
+            },
+            onTransitionSuccess: function onTransitionSuccess() {
+                console.log('Transition success');
+                endGroup();
+            }
+        };
     };
-}
+};
 
 exports.default = loggerPlugin;
 module.exports = exports['default'];
