ffb794cc30040cbcbee9a8508f1acd7bb7780212
Thomas Roch <thomas.c.roch@gmail.com>
2017-06-12 19:28:56 +0100

    chore: add rxjs-router5 package

---
+++ packages/rxjs-router5/README.md
@@ -0,0 +1,47 @@
+[![npm version](https://badge.fury.io/js/rxjs-router5.svg)](https://badge.fury.io/js/rxjs-router5)
+[![Build Status](https://travis-ci.org/router5/rxjs-router5.svg?branch=master)](https://travis-ci.org/router5/rxjs-router5?branch=master)
+
+# rxjs-router5
+
+[rxjs (RxJS 5+)](http://reactivex.io/rxjs/) integration with [router5](http://router5.github.io)
+
+```sh
+npm install --save rxjs-router5
+```
+
+### Usage
+
+_rxjs-router5_ exports a single function `createObservables`:
+
+```js
+import createRouter from 'router5';
+import createObservables from 'rxjs-router5';
+
+const router = createRouter([
+    { name: 'home', path: '/home' },
+    { name: 'about', path: '/about' }
+]);
+
+const {
+    route$,
+    routeNode,
+    transitionError$,
+    transitionRoute$
+} = createObservables(router)
+
+router.start();
+
+route$.map((route) => { /* ... */ })
+```
+
+### Available observables
+
+`createObservables` returns the following observables:
+- `route$`: an observable of your application route
+- `transitionRoute$`: an observable of the currently transitioning route
+- `transitionError$`: an observable of transition errors
+- `routeNode(nodeName)`: a function returning an observable of route updates for the specified node. See [understanding router5](http://router5.github.io/docs/understanding-router5.html).
+
+### Related
+
+- [xstream-router5](../xstream-router5)
---
+++ packages/rxjs-router5/test/transitionRoute$.js
@@ -0,0 +1,58 @@
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import { state1 } from './_helpers';
+import createObservables from '../modules';
+import createRouter, { constants } from 'router5';
+
+describe('transitionRoute$', () => {
+    it('should push new values to transitionRoute$ on transitionStart events', () => {
+        const router = createRouter();
+        const observables = createObservables(router);
+        const listener = { next: spy(), error() {}, complete: spy() };
+
+        observables.transitionRoute$.subscribe(listener);
+        router.invokeEventListeners(constants.TRANSITION_START, state1, null);
+
+        expect(listener.next).to.have.been.calledTwice;
+        expect(listener.next).to.have.been.calledWith(null);
+        expect(listener.next).to.have.been.calledWith(state1);
+
+        router.invokeEventListeners(constants.ROUTER_STOP);
+
+        expect(listener.complete).to.have.been.called;
+    });
+
+    it('should become null on a transition success', () => {
+        const router = createRouter();
+        const observables = createObservables(router);
+        const listener = { next: spy(), error() {}, complete: spy() };
+
+        observables.transitionRoute$.subscribe(listener);
+        router.invokeEventListeners(constants.TRANSITION_START, state1, null);
+        router.invokeEventListeners(constants.TRANSITION_SUCCESS, state1, null);
+
+        expect(listener.next).to.have.been.calledThrice;
+        expect(listener.next).to.have.been.calledWith(null);
+        expect(listener.next).to.have.been.calledWith(state1);
+        expect(listener.next).to.have.been.calledWith(null);
+
+        router.invokeEventListeners(constants.ROUTER_STOP);
+    });
+
+    it('should become null on a transition error', () => {
+        const router = createRouter();
+        const observables = createObservables(router);
+        const listener = { next: spy(), error() {}, complete() {} };
+
+        observables.transitionRoute$.subscribe(listener);
+        router.invokeEventListeners(constants.TRANSITION_START, state1, null);
+        router.invokeEventListeners(constants.TRANSITION_ERROR, state1, null);
+
+        expect(listener.next).to.have.been.calledThrice;
+        expect(listener.next).to.have.been.calledWith(null);
+        expect(listener.next).to.have.been.calledWith(state1);
+        expect(listener.next).to.have.been.calledWith(null);
+
+        router.invokeEventListeners(constants.ROUTER_STOP);
+    });
+});
