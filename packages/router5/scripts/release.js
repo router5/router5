473c658dee6471e67b3d6a0ce4dde105f3ece53d
Thomas Roch <thomas.c.roch@gmail.com>
2017-06-13 19:12:17 +0100

    refactor: remove bower config
    BREAKING CHANGE: router5 won't be released with bower beyond version 4

--- packages/router5/scripts/release.js
+++ packages/router5/scripts/release.js
@@ -10,7 +10,6 @@ var versionType = argv.major ? 'major' : (argv.minor ? 'minor' : 'patch');
 run('npm install');
 run('npm prune');
 var VERSION = run('npm --no-git-tag-version version ' + versionType, false);
-setBowerVersion(VERSION);
 
 run('npm run lint');
 run('npm run build');
@@ -31,10 +30,4 @@ function run(cmd, log) {
     var res = exec(cmd).toString();
     if (log && res) console.log(res);
     return res;
-}
-
-function setBowerVersion(version) {
-    var bowerConfig = require('../bower.json');
-    bowerConfig.version = version;
-    fs.writeFileSync(path.join(__dirname, '..', 'bower.json'), JSON.stringify(bowerConfig, null, '  ') + '\n');
 }
