dfb3e790b9a76e32f21f32a61ff294ff822083d9
Thomas Roch <thomas.c.roch@gmail.com>
2017-08-10 22:43:12 +0200

    chore: update dist

--- dist/router5.js
+++ dist/router5.js
@@ -959,6 +959,10 @@ var RouteNode = function () {
 
                 var val = params[withoutBrackets(p)];
 
+                if (Array.isArray(val)) {
+                    return val.length > 0;
+                }
+
                 return val !== undefined && val !== null;
             }).map(function (p) {
                 var val = params[withoutBrackets(p)];
