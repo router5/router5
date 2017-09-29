1ac6b6340441ba1e14202fdd600f561335dab3db
Andy Chou <acchou2@gmail.com>
2017-09-29 01:11:38 -0700

    feat: add typings for rxjs-router5 (#198)
    * Add type definition for rxjs-router5. Could be improved with better documentation of the observables...
    
    * Fix `routeNode` argument. Add comments to `RouterObservables`.

---
+++ packages/rxjs-router5/index.d.ts
@@ -0,0 +1,22 @@
+declare module "rxjs-router5" {
+    import * as Rx from "rxjs";
+    import { State, Router } from "router5";
+    interface RouterObservables {
+        /// route$: an observable of your application route
+        route$: Rx.Observable<State>;
+
+        /** 
+         * @param nodeName the name of a node
+         * @returns an observable of route updates for the specified node.
+         * See understanding router5: http://router5.github.io/docs/understanding-router5.html.
+         */
+        routeNode: (nodeName: string) => Rx.Observable<State>;
+
+        /// an observable of transition errors
+        transitionError$: Rx.Observable<any>;
+        
+        /// an observable of the currently transitioning route
+        transitionRoute$: Rx.Observable<State>;
+    }
+    export default function createObservables(router: Router): RouterObservables;
+}
