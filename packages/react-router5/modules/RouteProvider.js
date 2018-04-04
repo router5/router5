0ced8e32f7e570b31869c720786b726d2b67e235
Thomas Roch <thomas.c.roch@gmail.com>
2018-04-04 22:54:25 +0100

    feat: add RouteProvider and Route components, using React's new context API

---
+++ packages/react-router5/modules/RouteProvider.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import transitionPath from 'router5-transition-path'
+import { ifNot } from './utils'
+
+const emptyCreateContext = () => ({
+    Provider: _ => _,
+    Consumer: () => null
+})
+const createContext = React.createContext || emptyCreateContext
+
+const { Provider, Consumer: Route } = createContext({
+    route: null,
+    previousRoute: null
+})
+
+class RouteProvider extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        const { router } = props
+
+        this.state = {
+            route: router.getState()
+        }
+        this.listener = this.listener.bind(this)
+    }
+
+    listener(toState, fromState) {
+        this.setState({
+            route: toState,
+            previousRoute: fromState
+        })
+    }
+
+    componentDidMount() {
+        ifNot(
+            this.router.hasPlugin('LISTENERS_PLUGIN'),
+            '[react-router5][RouteProvider] missing listeners plugin'
+        )
+
+        this.listener = (toState, fromState) =>
+            this.setState({ previousRoute: fromState, route: toState })
+        this.router.addListener(this.listener)
+    }
+
+    componentWillUnmount() {
+        this.router.removeListener(this.listener)
+    }
+
+    render() {
+        return (
+            <Provider value={this.state.route}>{this.props.children}</Provider>
+        )
+    }
+}
+
+class RouteNodeConsumer extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        const { intersection } = transitionPath(
+            nextProps.routeContext.route,
+            nextProps.routeContext.previousRoute
+        )
+
+        return intersection === nextProps.nodeName
+    }
+
+    render() {
+        return this.props.children(this.props.routeContext)
+    }
+}
+
+RouteNodeConsumer.propTypes = {
+    nodeName: PropTypes.string.isRequired,
+    children: PropTypes.function.isRequired
+}
+
+function RouteNode(props) {
+    return (
+        <Route>
+            {routeContext => (
+                <RouteNodeConsumer {...props} routeContext={routeContext} />
+            )}
+        </Route>
+    )
+}
+
+RouteProvider.propTypes = {
+    router: PropTypes.object.isRequired,
+    children: PropTypes.node.isRequired
+}
+
+export { RouteProvider, Route, RouteNode }
--- packages/react-router5/modules/index.js
+++ packages/react-router5/modules/index.js
@@ -2,7 +2,17 @@ import BaseLink from './BaseLink'
 import routeNode from './routeNode'
 import RouterProvider from './RouterProvider'
 import withRoute from './withRoute'
+import { RouteProvider, Route, RouteNode } from './RouteProvider'
 
 const Link = withRoute(BaseLink)
 
-export { BaseLink, routeNode, RouterProvider, withRoute, Link }
+export {
+    BaseLink,
+    routeNode,
+    RouterProvider,
+    withRoute,
+    Link,
+    RouteProvider,
+    Route,
+    RouteNode
+}
