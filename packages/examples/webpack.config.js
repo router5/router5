c3a3eb7c200bb3ce762b75862497ef1135c7948a
Thomas Roch <thomas.c.roch@gmail.com>
2018-04-08 11:18:42 +0100

    chore: update dependencies

--- packages/examples/webpack.config.js
+++ packages/examples/webpack.config.js
@@ -1,34 +1,34 @@
 const path = require('path')
-const webpack = require('webpack')
 
 const argv = require('yargs').argv
 const app = argv.app || 'react-new-context-api'
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = {
-    entry: ['./apps/' + app + '/main.js'],
+console.log({
+    entry: './apps/' + app + '/main.js',
     output: {
         path: path.join(__dirname, 'build'),
         filename: isProd ? 'router5-' + app + '-example.js' : 'app.js'
     },
-    plugins: !isProd
-        ? []
-        : [
-              new webpack.LoaderOptionsPlugin({
-                  minimize: isProd,
-                  debug: false
-              }),
-              new webpack.optimize.UglifyJsPlugin({
-                  compressor: {
-                      warnings: false
-                  }
-              }),
-              new webpack.DefinePlugin({
-                  'process.env': {
-                      NODE_ENV: JSON.stringify('production')
-                  }
-              })
-          ],
+    mode: process.env.NODE_ENV || 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.join(__dirname, 'apps')
+            }
+        ]
+    }
+})
+
+module.exports = {
+    entry: './apps/' + app + '/main.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: isProd ? 'router5-' + app + '-example.js' : 'app.js'
+    },
+    mode: process.env.NODE_ENV || 'development',
     module: {
         rules: [
             {
