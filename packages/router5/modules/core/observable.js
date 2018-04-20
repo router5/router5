ad72bc5058102599af3f72bb4b1e6ee4afc2a291
Thomas Roch <thomas.c.roch@gmail.com>
2018-04-20 10:52:40 +0100

    feat: add subscribe function and observable compatibility, without the need to add a plugin

---
+++ packages/router5/modules/core/observable.js
@@ -0,0 +1,63 @@
+import $$observable from 'symbol-observable'
+
+function observerPlugin(router) {
+    let listeners = []
+
+    function unsubscribe(listener) {
+        if (listener) {
+            listeners = listeners.filter(l => l !== listener)
+        }
+    }
+
+    function subscribe(listener) {
+        listeners.concat(listener)
+
+        return unsubscribe(listener)
+    }
+
+    function observable() {
+        return {
+            subscribe(observer) {
+                if (typeof observer !== 'object' || observer === null) {
+                    throw new TypeError(
+                        'Expected the observer to be an object.'
+                    )
+                }
+
+                function listener() {
+                    if (observer.next) {
+                        observer.next(router.getState())
+                    }
+                }
+
+                listener()
+                const unsubscribe = subscribe(listener)
+                return { unsubscribe }
+            },
+
+            [$$observable]() {
+                return this
+            }
+        }
+    }
+
+    router.subscribe = subscribe
+    router[$$observable] = observable
+
+    return {
+        onTransitionSuccess: (toState, fromState) => {
+            listeners.forEach(listener =>
+                listener({
+                    route: toState,
+                    previousRoute: fromState
+                })
+            )
+        }
+    }
+}
+
+observerPlugin.pluginName = 'OBSERVABLE_PLUGIN'
+
+export default function withObservablePlugin(router) {
+    router.usePlugin(observerPlugin)
+}
