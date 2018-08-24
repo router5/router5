88036f73d609d65ada08ad349d8f16059f4dfd3c
Titouan CREACH <titouan.creach@klaxoon.com>
2018-08-24 11:09:47 +0200

    fix: wait for parent to be mounted before rendering children

--- packages/react-router5/modules/withRoute.js
+++ packages/react-router5/modules/withRoute.js
@@ -9,29 +9,34 @@ function withRoute(BaseComponent) {
             this.router = context.router
             this.state = {
                 previousRoute: null,
-                route: this.router.getState()
+                route: this.router.getState(),
+                mounted: false
             }
+        }
 
-            if (typeof window !== 'undefined') {
-                const listener = ({ route, previousRoute }) => {
-                    this.setState({ route, previousRoute })
-                }
-                this.unsubscribe = this.router.subscribe(listener)
+        componentDidMount() {
+            this.setState({
+                mounted: true
+            })
+            const listener = ({ route, previousRoute }) => {
+                this.setState({ route, previousRoute })
             }
+            this.unsubscribe = this.router.subscribe(listener)
         }
 
         componentWillUnmount() {
-            if (this.unsubscribe) {
-                this.unsubscribe()
-            }
+            this.unsubscribe()
         }
 
         render() {
-            return createElement(BaseComponent, {
-                ...this.props,
-                ...this.state,
-                router: this.router
-            })
+            return (
+                this.state.mounted &&
+                createElement(BaseComponent, {
+                    ...this.props,
+                    ...this.state,
+                    router: this.router
+                })
+            )
         }
     }
 
