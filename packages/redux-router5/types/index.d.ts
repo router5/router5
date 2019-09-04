6d5c371146c4169b63875ac420d71a5a39ae8a6c
Jan Dolezel <dolezel.jan@gmail.com>
2019-09-04 15:27:04 +0200

    Fix router5Reducer type to be compatible with redux reducers and make state more strict

--- packages/redux-router5/types/index.d.ts
+++ packages/redux-router5/types/index.d.ts
@@ -1,5 +1,5 @@
 import router5Middleware from './router5Middleware'
-import router5Reducer from './router5Reducer'
+import { router5Reducer, RouterState } from './router5Reducer'
 import createRouteNodeSelector from './routeNodeSelector'
 import * as actions from './actions'
 import * as actionTypes from './actionTypes'
@@ -7,6 +7,7 @@ import reduxPlugin from './reduxPlugin'
 export {
     router5Middleware,
     router5Reducer,
+    RouterState,
     actions,
     actionTypes,
     createRouteNodeSelector,
--- packages/redux-router5/types/router5Reducer.d.ts
+++ packages/redux-router5/types/router5Reducer.d.ts
@@ -1,15 +1,11 @@
-declare function router5Reducer(
-    state: {
-        route: any
-        previousRoute: any
-        transitionRoute: any
-        transitionError: any
-    },
-    action: any
-): {
-    route: any
-    previousRoute: any
-    transitionRoute: any
-    transitionError: any
+import { State } from 'router5'
+import { Reducer } from 'redux'
+
+export interface RouterState {
+    route: State | null
+    previousRoute: State | null
+    transitionRoute: State | null
+    transitionError: any | null
 }
-export default router5Reducer
+
+export const router5Reducer: Reducer<RouterState>
