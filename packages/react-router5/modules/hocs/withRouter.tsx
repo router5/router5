6c184c8f6bc199f98084e81fba608186d6feb268
lneves <lneves@memsql.com>
2020-04-21 22:30:33 +0100

    fix: - react-router5 - improve withRouter hoc types

--- packages/react-router5/modules/hocs/withRouter.tsx
+++ packages/react-router5/modules/hocs/withRouter.tsx
@@ -1,11 +1,11 @@
-import React, { SFC, ComponentType } from 'react'
+import React, { ComponentType } from 'react'
 import { Router } from 'router5'
 import { routerContext } from '../context'
 
 function withRouter<P>(
     BaseComponent: ComponentType<P & { router: Router }>
-): SFC<Exclude<P, 'router'>> {
-    return function WithRouter(props) {
+): ComponentType<Omit<P, 'router'>> {
+    return function WithRouter(props: P) {
         return (
             <routerContext.Consumer>
                 {router => <BaseComponent {...props} router={router} />}
